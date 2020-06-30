3891f9d648a3e6e72ce5530c935a49b146b99b64
Dimas <ulybin-dmitriy@rambler.ru>
2020-06-30 10:19:20 +0500

    commit 2

---
+++ backup/unit1.pas
@@ -0,0 +1,48 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+var a,b:real;
+s:string;
+begin
+a:=StrtoFloat(edit1.Text);
+
+b:=StrtoFloat(edit2.Text);
+s:=FloattoStr(a/b*16.67);
+
+edit3.Text:=s;
+
+     end;
+end.
+
--- unit1.pas
+++ unit1.pas
@@ -5,10 +5,20 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    procedure Button1Click(Sender: TObject);
+    procedure Edit1KeyPress(Sender: TObject; var Key: char);
+    procedure Edit2KeyPress(Sender: TObject; var Key: char);
   private
 
   public
@@ -22,5 +32,43 @@ implementation
 
 {$R *.lfm}
 
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+var a,b:real;
+s:string;
+begin
+a:=StrtoFloat(edit1.Text);
+
+b:=StrtoFloat(edit2.Text);
+s:=FloattoStr(a/b*16.67);
+
+edit3.Text:=s;
+
+     end;
+procedure TForm1.Edit1KeyPress(Sender: TObject; var Key: char);
+
+
+begin
+if not (Key in [#8,',','0'..'9'])
+then
+begin
+MessageDlg(' Введите правильные данные',mtInformation,[mbOk],0);
+Key:=#0;
+
+end;
+end;
+
+procedure TForm1.Edit2KeyPress(Sender: TObject; var Key: char);
+
+
+begin
+if not (Key in [#8,',','0'..'9'])
+then
+begin
+MessageDlg(' Введите правильные данные',mtInformation,[mbOk],0);
+Key:=#0;
+end;
+end;
 end.
 
